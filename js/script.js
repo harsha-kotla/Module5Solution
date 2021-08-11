f8cba8ed9e9300be6dc75f7a6427324fc2cd7fb1
harsha-kotla <72369147+harsha-kotla@users.noreply.github.com>
2021-08-11 16:41:46 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -123,7 +123,7 @@ function buildAndShowHomeHTML (categories) {
         },
         false)
       
-      {{randomCategoryShortName}} = chosenCategoryShortName
+      
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
