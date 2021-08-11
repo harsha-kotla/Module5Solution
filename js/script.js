cff9389900c024e00f98584239154b0c5f3186dc
harsha-kotla <72369147+harsha-kotla@users.noreply.github.com>
2021-08-11 15:43:37 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,8 +117,8 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var homeHtmlToInstertIntoMainPage = $dc.loadMenuItems(chosenCategoryShortName)
-  console.log(chosenCategoryShortName)
+      insertHtml("#main-content", homeHtmlUrl);
+      
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
